8a53743be393adf4076725bcc5048b1b7b58f226
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2020-05-27 12:12:37 +0200

    Update FlashLoanReceiverBase.sol

--- contracts/aave/FlashLoanReceiverBase.sol
+++ contracts/aave/FlashLoanReceiverBase.sol
@@ -1,6 +1,6 @@
 // Original: https://github.com/aave/aave-protocol/blob/master/contracts/flashloan/base/FlashLoanReceiverBase.sol
 
-pragma solidity ^0.5.0;
+pragma solidity >0.6.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
